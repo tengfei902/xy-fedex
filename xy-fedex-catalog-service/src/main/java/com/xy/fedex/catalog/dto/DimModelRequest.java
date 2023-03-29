773298c023bc47d4edfe6b85462d4852316ba29c
tengfei <331265866@qq.com>
2023-03-29 23:25:28 +0800

    add

--- xy-fedex-catalog-service/src/main/java/com/xy/fedex/catalog/dto/DimModelRequest.java
+++ xy-fedex-catalog-service/src/main/java/com/xy/fedex/catalog/dto/DimModelRequest.java
@@ -1,15 +1,12 @@
 package com.xy.fedex.catalog.dto;
 
-import com.sun.istack.internal.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
 public class DimModelRequest {
-    @NotNull
     private Long appId;
-    @NotNull
     private Long dimId;
     private Long modelId;
 }
