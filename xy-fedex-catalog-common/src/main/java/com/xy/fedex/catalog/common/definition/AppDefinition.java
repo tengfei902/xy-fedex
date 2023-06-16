d8df907445f0cf07fca35f8ae2cbaf1a0659acd5
tengfei <331265866@qq.com>
2023-06-16 09:26:49 +0800

    add

--- xy-fedex-catalog-common/src/main/java/com/xy/fedex/catalog/common/definition/AppDefinition.java
+++ xy-fedex-catalog-common/src/main/java/com/xy/fedex/catalog/common/definition/AppDefinition.java
@@ -1,7 +1,7 @@
 package com.xy.fedex.catalog.common.definition;
 
-import com.xy.fedex.catalog.common.enums.DimType;
-import com.xy.fedex.catalog.common.enums.MetricType;
+import com.xy.fedex.catalog.common.definition.field.Dim;
+import com.xy.fedex.catalog.common.definition.field.Metric;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -15,22 +15,4 @@ public class AppDefinition implements Serializable {
     private List<Long> modelIds;
     private List<Metric> metrics;
     private List<Dim> dims;
-
-    @Data
-    public static class Metric implements Serializable {
-        private Long metricId;
-        private String metricCode;
-        private String metricName;
-        private String metricComment;
-        private MetricType metricType;
-    }
-
-    @Data
-    public static class Dim implements Serializable {
-        private Long dimId;
-        private String dimCode;
-        private String dimName;
-        private String dimComment;
-        private DimType dimType;
-    }
 }
--- xy-fedex-catalog-common/src/main/java/com/xy/fedex/catalog/common/definition/ModelDefinition.java
+++ xy-fedex-catalog-common/src/main/java/com/xy/fedex/catalog/common/definition/ModelDefinition.java
@@ -1,14 +1,15 @@
 package com.xy.fedex.catalog.common.definition;
 
-import com.xy.fedex.catalog.common.definition.field.AdvanceCalculate;
+import com.xy.fedex.catalog.common.definition.field.DimModel;
+import com.xy.fedex.catalog.common.definition.field.MetricModel;
 import lombok.Data;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
+/**
+ * @author tengfei
+ */
 @Data
 public class ModelDefinition implements Serializable {
     private Long modelId;
@@ -18,38 +19,6 @@ public class ModelDefinition implements Serializable {
     private String condition;
     private String version;
     private Map<String,String> modelProp;
-    private List<Metric> metrics;
-    private List<Dim> dims;
-
-    private transient Map<String,Dim> dimMap;
-
-    public Dim getDim(String dimCode) {
-        if(Objects.isNull(dimMap)) {
-            synchronized (this) {
-                dimMap = this.dims.stream().collect(Collectors.toMap(Dim::getDimCode, Function.identity()));
-            }
-        }
-        return dimMap.get(dimCode);
-    }
-
-    @Data
-    public static class Metric implements Serializable {
-        private Long metricModelId;
-        private Long metricId;
-        private String metricCode;
-        private String metricName;
-        private String metricComment;
-        private String formula;
-        private AdvanceCalculate advanceCalculate;
-    }
-
-    @Data
-    public static class Dim implements Serializable {
-        private Long dimModelId;
-        private Long dimId;
-        private String dimCode;
-        private String dimName;
-        private String dimComment;
-        private String formula;
-    }
+    private List<MetricModel> metrics;
+    private List<DimModel> dims;
 }
